b9d28da8388636d55589c5a535c8c05b35f19578
SIDDHI-1105 <your-email@example.com>
2026-06-08 14:25:39 +0530

    patterns

---
+++ rightside_inverted tingla.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int n = 8;
+int main(){
+    for(int i = 0 ; i < n/2; i++){
+        for(int j = 0; j <= i; j++){
+            cout << "*" ;
+        }
+    // for(int i = 4 ; i < n i--){
+    //     for(int j = 0; j <= i; j++){
+    //         cout << "*" ; 
+    //     }  
+    // }
+    cout<< endl;   
+    }
+
+}
+
