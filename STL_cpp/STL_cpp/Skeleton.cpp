67a928ad7864e0074c985dea4c47a1f15841a974
SIDDHI-1105 <your-email@example.com>
2026-06-15 20:29:53 +0530

    Removed old STL files

--- STL_cpp/STL_cpp/Skeleton.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>   // Includes all standard C++ libraries
-using namespace std;       // Lets us use standard names without "std::"
-
-int main() {
-    int a;                 // Declare an integer variable
-    cin >> a;              // Take input from user
-    cout << a << endl;     // Print the value of 'a'
-    return 0;              // End program successfully
-}
--- STL_cpp/STL_cpp/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
- vector<int > distans(4);
-    distans.reserve(10);
-    distans.push_back(10);
-    distans.push_back(20);
-    distans.push_back(30);
-distans.push_back(15);
-    distans.push_back(55);
